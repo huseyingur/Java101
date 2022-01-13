58c6a1ee186b4daaa708c5098a28a16229a5ac9f
huseyingur <gur.huseyin@gmail.com>
2022-01-14 00:08:19 +0300

    First Commmit Project empty

---
+++ src/Metotlar/DesMet.java
@@ -0,0 +1,34 @@
+package Metotlar;
+import java.util.Scanner;
+
+public class DesMet {
+
+    static int eksi(int n,int temp) {
+        System.out.print(n + " ");
+        if (n <= 0) {
+            return arti(n+5, temp);
+        } else {
+            return eksi(n - 5, temp);
+        }
+    }
+
+    static int arti(int n,int temp){
+        System.out.print(n + " ");
+        if(n==temp){
+            return 0;
+        }
+        else {
+            return arti(n+5,temp);
+        }
+    }
+    public static void main(String[] args){
+
+        int n,temp;
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Bir Sayı giriniz: ");
+        n= inp.nextInt();
+        temp=n;
+        eksi(n,temp);
+    }
+}

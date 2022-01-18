bc2b690d04bb8637612b4a7a389738251791858c
huseyingur <gur.huseyin@gmail.com>
2022-01-18 22:26:53 +0300

    ...

---
+++ src/Diziler/Siralama.java
@@ -0,0 +1,28 @@
+package Diziler;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class Siralama {
+    public static void main(String[] args) {
+
+        Scanner inp = new Scanner(System.in);
+        System.out.print(" Dizi boyutunu giriniz: ");
+        int boyut = inp.nextInt();
+
+        int[] list = new int[boyut];
+
+        for (int i =0; i <= boyut-1;i++){
+            System.out.print(i+1 +". Sayıyı Giriniz: ");
+            list[i] = inp.nextInt();
+        }
+
+
+
+
+
+        System.out.println(Arrays.toString(list));
+
+    }
+}
